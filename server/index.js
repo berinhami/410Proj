680098fc10e6b27f45974d304d92bc4d28cb1a61
unknown <berinhami@gmail.com>
2022-01-28 13:00:40 -0700

    first draft of index

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const { text } = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.text())
+
+app.get('/', function (req, res){
+    res.send('Hello World')
+})
+
+app.post('/', (req, res) =>{
+
+    if(req.get('content-type') !== 'application/json' && req.get('content-type') !== 'text/plain'){
+		res.status(400).send(`I will not process this`)
+    }
+    else{
+        console.log(req.get('content-type'))
+        console.log('Body: ', req.body)
+        res.send('OK')
+    }
+})
+
+app.listen(port)
